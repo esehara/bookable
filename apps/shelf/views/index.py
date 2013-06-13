2430f2b8c53ed524358a65227f7bc4781758d91f
esehara <esehara@gmail.com>
2013-06-13 17:27:30 +0900

    FIX: 冊数の調整

--- apps/shelf/views/index.py
+++ apps/shelf/views/index.py
@@ -6,10 +6,10 @@ from apps.shelf.models import Book
 def index(request):
     d = {}
     d['favorite_books'] = Book.objects.filter(
-        users__gt=100).order_by('?')[:3]
+        users__gt=100).order_by('?')[:2]
     d['normal_books'] = Book.objects.filter(
-        users__lt=100, users__gt=50).order_by('?')[:3]
+        users__lt=100, users__gt=50).order_by('?')[:2]
     d['newbee_books'] = Book.objects.filter(
-        users__lt=50).order_by('?')[:3]
+        users__lt=50).order_by('?')[:2]
     return render(
         request, 'index.html', d)
