69fc5de20c5826c6c64f46115889968d5afb1b09
esehara <esehara@gmail.com>
2013-06-16 13:03:32 +0900

    BAD: ALLOWED_HOSTS use all.

--- bookable/settings.py
+++ bookable/settings.py
@@ -145,7 +145,7 @@ if DEBUG:
     INSTALLED_APPS += ('debug_toolbar', )
 
 if not DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookable.jp']
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
