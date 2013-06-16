d70af7fcafe3791293eecab58bc846761e4bad4e
esehara <esehara@gmail.com>
2013-06-16 10:17:45 +0900

    FIX: use debug toolbar;

--- bookable/settings.py
+++ bookable/settings.py
@@ -94,9 +94,26 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+if DEBUG:
+    INTERNAL_IPS += ('0.0.0.0', )
 
 ROOT_URLCONF = 'bookable.urls'
 
@@ -125,6 +142,9 @@ INSTALLED_APPS = (
     'django_jenkins',
     'django.contrib.admin',
 ) + PROJECT_APPS
+
+if DEBUG:
+    INSTALLED_APPS += ('debug_toolbar', )
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
