80e94b5a5d7a113571fb225218394bd2aad9bac2
esehara <esehara@gmail.com>
2013-06-21 23:51:16 +0900

    ADD: migration files.

---
+++ apps/shelf/migrations/0007_auto__add_field_keyword_times__chg_field_keyword_text.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Keyword.times'
+        db.add_column(u'shelf_keyword', 'times',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
+
+
+        # Changing field 'Keyword.text'
+        db.alter_column(u'shelf_keyword', 'text', self.gf('django.db.models.fields.TextField')(null=True))
+
+    def backwards(self, orm):
+        # Deleting field 'Keyword.times'
+        db.delete_column(u'shelf_keyword', 'times')
+
+
+        # User chose to not deal with backwards NULL issues for 'Keyword.text'
+        raise RuntimeError("Cannot reverse this migration. 'Keyword.text' and its values cannot be restored.")
+
+    models = {
+        u'shelf.book': {
+            'Meta': {'ordering': "['created_at']", 'object_name': 'Book'},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'detail': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True'}),
+            'is_mecab': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'price': ('django.db.models.fields.IntegerField', [], {}),
+            'text': ('django.db.models.fields.TextField', [], {'null': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'users': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'via': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'shelf.keyword': {
+            'Meta': {'object_name': 'Keyword'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'times': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'shelf.keywordtobook': {
+            'Meta': {'object_name': 'KeywordToBook'},
+            'book': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shelf.Book']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keyword': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['shelf.Keyword']"})
+        }
+    }
+
+    complete_apps = ['shelf']
