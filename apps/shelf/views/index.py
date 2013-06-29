6a083a087179bfd9e3c0617745f513cafce02cf3
esehara <esehara@gmail.com>
2013-06-29 13:31:11 +0900

    FIX: cache time fix.

--- apps/shelf/views/index.py
+++ apps/shelf/views/index.py
@@ -8,7 +8,7 @@ def index(request):
     page = request.GET.get('page', '1')
     
     cache_key = 'index_cache_dict_page_%s' % page
-    cache_time = 300
+    cache_time = 180
     d = cache.get(cache_key)
 
     if not d:
