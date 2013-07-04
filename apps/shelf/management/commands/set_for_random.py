069af374c25e5f9e78eedb3a8de1f451ed8b83eb
esehara <esehara@gmail.com>
2013-07-04 21:22:27 +0900

    FIX: rename set_for_random.py

--- apps/shelf/management/commands/set_for_random.py
+++ apps/shelf/management/commands/set_for_random.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand
-from apps.shelf.models import Book
+from apps.shelf.models import (Book, Keyword)
 
 
 class Command(BaseCommand):
@@ -12,3 +12,9 @@ class Command(BaseCommand):
             book.set_for_random()
             print "No.", book.pk
             print "for Random:", book.for_random
+
+        keywords = Keyword.objects.filter(for_random=None)
+        for keyword in keywords:
+            keyword.set_for_random()
+            print "Keyword:", keyword.name
+            print "for Random", keyword.for_random
